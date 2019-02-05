1b3bfac15636886c29edc80e5214b72e3c7e424d
Chen Yu Pao <windperson@gmail.com>
2019-02-05 10:16:55 +0800

    Add project files.

---
+++ WatchApp1/WatchApp1.WatchOSExtension/ExtensionDelegate.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+using Foundation;
+using WatchKit;
+
+namespace WatchApp1.WatchOSExtension
+{
+    public class ExtensionDelegate : WKExtensionDelegate
+    {
+        public ExtensionDelegate()
+        {
+        }
+    }
+}
+
